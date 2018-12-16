04b4a8bf86a268c21d7dff8db2cbc26cac0e6296
johannes-jansson <johannes.e.jansson@gmail.com>
2018-12-16 21:30:23 +0100

    fine tuning last change

--- keyboards/lets_split/keymaps/janzon/keymap.c
+++ keyboards/lets_split/keymaps/janzon/keymap.c
@@ -50,8 +50,8 @@ extern keymap_config_t keymap_config;
 #define KC_SE_HALF KC_TILD
 #define KC_SE_ACUT KC_EQL
 #define KC_SE_GRAV KC_PLUS
-#define KC_SE_MORE S(KC_NUBS)
-#define KC_SE_LESS KC_NUBS
+#define KC_SE_GT S(KC_GRV)
+#define KC_SE_LT KC_GRV
 #define KC_SE_PLUS KC_MINS
 #define KC_SE_ASTR KC_PIPE
 #define KC_SE_QUES S(KC_MINS)
@@ -61,8 +61,7 @@ extern keymap_config_t keymap_config;
 #define KC_SE_COLN S(KC_DOT)
 #define KC_SE_MINS KC_SLSH
 #define KC_SE_UNDS S(KC_SLSH)
-#define KC_SE_LT KC_NUBS
-#define KC_SE_GT S(KC_NUBS)
+
 
 /* TODOS:
  * [x] Decide on shift
@@ -85,6 +84,10 @@ enum custom_keycodes {
   FN,
   SYS,
   ANSIKEY,
+  M_SORT,
+  M_ID,
+  ANDREE,
+  MY_SCREENSHOT,
 };
 
 // Fillers to make layering more clear
@@ -98,20 +101,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------   |------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   |   H  |   J  |   K  |   L  |   Ö  |  Ä   |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   |   N  |   M  |   ,  |   .  |   /  |  Alt |
  * |------+------+------+------+------+------+   |------+------+------+------+------+------|
  * | Shift| Shift| Shift|  CMD |  Fn  | Shift|   | Space| Alt  | Ctrl | Shift| Shift| Shift|
  * `-----------------------------------------'   `-----------------------------------------'
  *  alt acts as enter when tapped
  *  fn acts as tab when tapped
  *  middle shift acts as backspace when tapped
- *  , . / becomes < > ? when shift is pressed, as usual with ANSI
+ *  , . / becomes ; > ? when shift is pressed
  */
 
 [_QWERTY] = LAYOUT_ortho_4x12(
     KC_MYTAB,    KC_Q,    KC_W,     KC_E,    KC_R,      KC_T,        KC_Y,      KC_U,    KC_I,    KC_O,       KC_P, KC_ARING, \
       KC_ESC,    KC_A,    KC_S,     KC_D,    KC_F,      KC_G,        KC_H,      KC_J,    KC_K,    KC_L,    KC_OUML,  KC_AUML, \
-     KC_RSFT,    KC_Z,    KC_X,     KC_C,    KC_V,      KC_B,        KC_N,      KC_M, KC_COMM,  KC_DOT,    ANSIKEY,  KC_RSFT, \
+     KC_RSFT,    KC_Z,    KC_X,     KC_C,    KC_V,      KC_B,        KC_N,      KC_M, KC_COMM,  KC_DOT,    ANSIKEY,  KC_RALT, \
      KC_RSFT,   RESET,  KC_RSFT, KC_LGUI, KC_MYFN,  KC_MYSFT,      KC_SPC,  KC_MYALT, KC_RCTL, KC_RSFT,    KC_RSFT,  KC_RSFT  \
 ),
 // SH_MON is swap hands
@@ -128,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'   `-----------------------------------------'
  */
 [_FN] = LAYOUT_ortho_4x12( \
-        KC_GRV,    KC_EXLM,  KC_SE_AT, KC_SE_LCBR, KC_SE_RCBR, KC_SE_AMPR, KC_SE_ASTR,    KC_7,      KC_8,    KC_9, KC_SE_PLUS,  KC_SE_EQL, \
+    KC_SE_GRAV,    KC_EXLM,  KC_SE_AT, KC_SE_LCBR, KC_SE_RCBR, KC_SE_AMPR, KC_SE_ASTR,    KC_7,      KC_8,    KC_9, KC_SE_PLUS,  KC_SE_EQL, \
     KC_SE_TILD, KC_SE_CIRC, KC_SE_DLR, KC_SE_LPRN, KC_SE_RPRN, KC_SE_QUOT, KC_SE_DQUO,    KC_4,      KC_5,    KC_6, KC_SE_MINS, KC_SE_UNDS, \
     KC_SE_PIPE,    KC_HASH,  KC_SE_LT, KC_SE_LBRC, KC_SE_RBRC,   KC_SE_GT,    KC_PERC,    KC_1,      KC_2,    KC_3, KC_SE_BSLS,    _______, \
          RESET,  TO(_GAME),   _______,    _______,    _______,    _______,    _______, _______, KC_MYZERO,    KC_0,    _______,    _______  \
@@ -147,10 +150,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_SYS] = LAYOUT_ortho_4x12(
-    KC_MYTAB, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,        _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, \
-   TO(_GAME), KC_MRWD, KC_MPLY, KC_MFFD, _______, _______,        KC_BTN1, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, \
-       RESET, _______, _______, _______, _______, _______,        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, _______, \
-     _______, _______, _______, _______, _______, _______,        KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______ \
+    KC_MYTAB, KC_VOLD, KC_MUTE, KC_VOLU,        M_SORT,    M_ID,        _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, \
+   TO(_GAME), KC_MRWD, KC_MPLY, KC_MFFD, MY_SCREENSHOT,  ANDREE,        KC_BTN1, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, \
+       RESET, _______, _______, _______,       _______, _______,        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, _______, \
+     _______, _______, _______, _______,       _______, _______,        KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______ \
 ),
 
 /* Game
@@ -204,7 +207,7 @@ bool lisinges_variable = false;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   uint8_t shift_pressed = get_mods() & ((MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)));
   uint8_t alt_pressed = get_mods() & ((MOD_BIT(KC_LALT) | MOD_BIT(KC_RALT)));
-  // The following five cases map alt hjkl to arrow keys 
+
   switch (keycode) {
     case ANSIKEY: {
       if (record->event.pressed)
@@ -223,6 +226,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       return false;
     }
+    // The following five cases map alt hjkl to arrow keys 
     case KC_MYALT:
       if (record->event.pressed) {
         lisinges_variable = true;
@@ -286,14 +290,40 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KC_E:
       if (alt_pressed) {
         if (record->event.pressed) {
-          SEND_STRING(SS_LALT("00E9"));
-          return false;
-          break;
+          /* register_code(KC_SE_ACUT); */
+          register_code(KC_E);
+        } else {
+          /* unregister_code(KC_SE_ACUT); */
+          unregister_code(KC_E);
         }
       } 
       return true;
       break;
-
+    case ANDREE:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_EQUAL)SS_TAP(X_E));
+      }
+      return false;
+      break;
+    case MY_SCREENSHOT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL(SS_LGUI(SS_LSFT(SS_TAP(X_4)))));
+      }
+      return false;
+      break;
+    case M_SORT:
+      if (record->event.pressed) {
+        SEND_STRING(".sort* " SS_LALT(SS_LSFT(SS_TAP(X_8))) " created?on> /1 " SS_LALT(SS_LSFT(SS_TAP(X_9))) " (");
+      }
+      return false;
+      break;
+    case M_ID:
+      if (record->event.pressed) {
+        /* sends the string "_id": ObjectId("") */
+        SEND_STRING(SS_LSFT(SS_TAP(X_2)) "?id" SS_LSFT(SS_TAP(X_2)) "> ObjectId*" SS_LSFT(SS_TAP(X_2)) SS_LSFT(SS_TAP(X_2)) "(");
+      }
+      return false;
+      break;
     // The following two cases handle layer switching
     case QWERTY:
       if (record->event.pressed) {
