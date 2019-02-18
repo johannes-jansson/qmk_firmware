dbe620f4a14de7da2e931472631cc84162c82670
johannes-jansson <johannes.e.jansson@gmail.com>
2019-02-18 17:33:59 +0100

    updated wiring

--- keyboards/lets_split/keymaps/janzon/config.h
+++ keyboards/lets_split/keymaps/janzon/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_LEFT
 // #define MASTER_RIGHT
 #define EE_HANDS
-#define FLIP_HALF
+// #define FLIP_HALF
 
 #endif
 
